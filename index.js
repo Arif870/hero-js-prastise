9c2b47c28f827b9ec787f1c036827b3abbf019c3
Arif Uz Zaman <52405658+Arif870@users.noreply.github.com>
2021-08-08 17:03:28 +0600

    problem up 15 solved

--- index.js
+++ index.js
@@ -1,233 +1,140 @@
-// factorial number calculation  by increment
+// // 1. Checklist work
+// let name = "Arif-uz-zaman";
+// let age = 24;
+// let isTrue = true;
 
-let fact = 1;
-let number = 4;
-for (i = 1; i <= number; i++) {
-  fact = fact * i;
-}
-console.log(`The factorial of ${number} is ${fact}.`);
+// // 2. Checklist work
 
-function fact(number) {
-  let fact = 1;
-  for (let i = 1; i <= number; i++) {
-    fact *= i;
-  }
-  return fact;
-}
+// let test1 = 12;
+// const test2 = 20;
+// var test3 = 22;
 
-// factorial number calculation  by dicrement
+// test1 = 30;
+// // const value can not be changed
+// //test2 = 30;
+// test3 = 78;
 
-let factorial = fact(5);
+// console.log(test1);
 
-console.log(factorial);
+// // 3. Checklist work
 
-function fact(number) {
-  let fact = 1;
-  for (let i = number; i >= 1; i--) {
-    fact *= i;
-  }
-  return fact;
-}
+// let add = test1 + test2;
+// console.log(add);
 
-console.log(fact(5));
+// // 7. Checklist work
 
-for (i = 10; i >= 1; i--) {
-  console.log(i);
-}
+// for (let i = 7; i <= 19; i++) {
+//   console.log(i);
+// }
 
-// celciouis to farenhite
+// // 8 . Checklist work
 
-function celcious(cel) {
-  return cel * (9 / 5) + 32;
-}
+// let arr = [1, 3, 4, 5, 6, 6];
 
-let findCelcius = celcious(10);
+// // arr.splice(3, 1, 10);
+// // console.log(arr.length);
+// // console.log(arr);
 
-console.log(findCelcius);
+// // arr.push(100);
+// // arr.pop();
+// // arr.shift();
+// // arr.unshift(100);
 
-// farenheight to celcious
+// for (let newArr of arr) {
+//   console.log(newArr);
+// }
 
-function farenheight(far) {
-  return ((far - 32) * 5) / 9;
-}
+// // 10. Checklist Work
 
-let showFrenheight = farenheight(10);
-console.log(showFrenheight);
+// let arr = [10, 40, 80, 400, 3020, 402];
 
-// grade calculation
+// for (let myArr of arr) {
+//   if (myArr >= 80) {
+//     console.log(myArr);
+//   }
+// }
 
-function grade(number) {
-  if (number < 0 && number > 100) {
-    console.log("Please enter valid number ");
-  } else if (number >= 0 && number <= 33) {
-    console.log("You  failed !");
-  } else if (number >= 34 && number <= 50) {
-    console.log("Your grade is B");
-  } else if (number >= 51 && number <= 60) {
-    console.log("Your grade is A-");
-  } else if (number >= 61 && number <= 80) {
-    console.log("Your grade is A");
-  } else if (number >= 81 && number <= 100) {
-    console.log("Your grade is A+");
+// // 11  . Checklist Work
+
+// function multy(a, b, c) {
+//   return a * b * c;
+// }
+
+// let multiplication = multy(10, 10, 10);
+// console.log(multiplication);
+
+// // 12  . Checklist Work
+
+// let obj = {
+//   name: "Arif",
+//   age: 42,
+// };
+
+// obj["age"] = 24;
+
+// console.log(obj);
+
+// // 13  . Checklist Work
+
+// function feetToInchi(feet) {
+//   return feet * 12;
+// }
+
+// let myVal = feetToInchi(5);
+
+// console.log(`${myVal} inchies`);
+
+// function centimeeterToMeter(centimeter) {
+//   return centimeter / 100;
+// }
+// let myMeter = centimeeterToMeter(200);
+// console.log(myMeter + " meter");
+
+// // 14  . Checklist Work
+
+// function paperRequirements(firstBook, secondBook, thirdBook) {
+//   let paperFristBook = 100;
+//   let paperSecondBook = 200;
+//   let paperThirdBook = 300;
+
+//   let totalFirstBookPaper = firstBook * paperFristBook;
+//   let totalSecondBookPaper = secondBook * paperSecondBook;
+//   let totalThirdBookPaper = thirdBook * paperThirdBook;
+
+//   return totalFirstBookPaper + totalSecondBookPaper + totalThirdBookPaper;
+// }
+
+// let totalPapers = paperRequirements(1, 1, 2);
+// console.log("Total Papers Requirements :", totalPapers);
+
+// // 15 . Checklist Work
+
+// let arr = ["Arif", "uz", "zaman", "khan", "sumaiya"];
+
+// function bestFriend(friends) {
+//   let word = "";
+//   for (let i = 0; i < friends.length; i++) {
+//     if (word.length < friends[i].length) {
+//       word = friends[i];
+//     }
+//   }
+//   return word;
+// }
+
+// console.log(bestFriend(arr));
+
+// 16  . Checklist Work
+
+let numbers = [1, 24, 45, 43, -10, 50];
+
+let positiveArray = [];
+
+for (let i = 0; i <= numbers.length; i++) {
+  if (numbers[i] < 0) {
+    break;
   } else {
-    console.log("Please enter valid number");
-  }
-}
-grade(90);
-
-// simple interest calculation
-
-function interest(p, r, t) {
-  let interest = (p * r * t) / 100;
-
-  totalTaka = interest + p;
-  return totalTaka;
-}
-
-totalInterest = interest(500, 5, 3);
-
-console.log(totalInterest);
-
-// swapping
-
-let num1 = 10;
-let num2 = 20;
-
-let swap;
-
-console.log(num1, num2);
-
-swap = num1;
-num1 = num2;
-num2 = swap;
-
-console.log(num1, num2);
-
-// destracture
-
-[num1, num2] = [num2, num1];
-
-console.log(num1, num2);
-
-// largest number and lowest number
-
-let largest = Math.max(20, 30, 10);
-
-function largetNumber(a, b, c) {
-  if (a > b && a > c) {
-    return a;
-  } else if (b > a && b > c) {
-    return b;
-  } else {
-    return c;
+    positiveArray.push(numbers[i]);
   }
 }
 
-let myNumber = largetNumber(30, 300, 2010);
-console.log("largest number is ", myNumber);
-
-function lowest(x, y, z) {
-  if (x < y && x < z) {
-    return x;
-  } else if (y < x && y < z) {
-    return y;
-  } else {
-    return z;
-  }
-}
-
-let myNUmber = lowest(5, 4, 2);
-console.log("Lowest number is ", myNUmber);
-
-// array sametion
-
-let arrsum = 0;
-
-let arr = [1, 2, 4, 5, 6, 10];
-
-for (i = 0; i < arr.length; i++) {
-  arrsum = arrsum + arr[i];
-}
-
-console.log(arrsum);
-
-// array sametion by function
-
-let myArray = [1, 2, 4, 5, 6, 10];
-
-function arrSum(anyArr) {
-  let arrSumTotal = 0;
-  for (let i = 0; i < anyArr.length; i++) {
-    arrSumTotal = arrSumTotal + anyArr[i];
-  }
-
-  return arrSumTotal;
-}
-let sumFun = arrSum(myArray);
-
-console.log(sumFun);
-
-// Find the largest number of and array
-
-let arr = [39, 23, 43, 12, 44, 14, 43, 132, -12];
-
-function largestNumber(number) {
-  let largest = number[0];
-
-  for (i = 0; i < number.length; i++) {
-    if (number[i] > largest) {
-      largest = number[i];
-    }
-  }
-  return largest;
-}
-
-let largestArrNumber = largestNumber(arr);
-
-console.log(largestArrNumber);
-
-// Lowest Number
-
-function lowestNumber(numberLow) {
-  let lowest = numberLow[0];
-
-  for (let i = 0; i < numberLow.length; i++) {
-    if (numberLow[i] < lowest) {
-      lowest = numberLow[i];
-    }
-  }
-
-  return lowest;
-}
-
-let lowestArr = lowestNumber(arr);
-
-console.log(lowestArr);
-
-// fibonacci number
-
-let fibo = [0, 1];
-
-for (i = 2; i <= 10; i++) {
-  fibo[i] = fibo[i - 1] + fibo[i - 2];
-}
-console.log(fibo);
-
-// fucntional way
-
-function fibonaciNumber(number) {
-  if (typeof number != "number") {
-    return "Please give a number";
-  }
-  if (number < 2) {
-    return "Please give a number greater then 2";
-  }
-  let fibonacci = [0, 1];
-  for (let i = 2; i <= number; i++) {
-    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-  }
-  return fibonacci;
-}
-
-let n = fibonaciNumber(10);
-console.log(n);
+console.log(positiveArray);
